cbea423c0e6c9cdad3673d2fc76f2d6058d8b985
Frode Aarebrot <frode@aarebrot.net>
2024-04-30 11:23:04 -0600

    Call FplJob.Run() from API to attempt to use Azure always-on to run webjob on a linux host

---
+++ FplBot.Api/Controllers/RootController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FplBot.Api.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class RootController(FplJob job) : ControllerBase
+    {
+        private readonly FplJob job = job;
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await this.job.Run(null, null);
+
+            return this.Ok();
+        }
+    }
+}
---
+++ FplBot.Api/Program.cs
@@ -0,0 +1,52 @@
+using Azure.Storage.Blobs;
+using FplBot.Options;
+using FplBot.Services;
+
+namespace FplBot.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddScoped<FplJob>();
+
+            builder.Services.AddScoped<IFplService, FplService>();
+            builder.Services.AddScoped<IOutputService, OutputService>();
+            builder.Services.AddScoped<IPersistenceService, PersistenceService>();
+            builder.Services.AddScoped<IEmailService, EmailService>();
+            builder.Services.AddScoped<IAttachmentService, AttachmentService>();
+            builder.Services.AddScoped<Logging.ILogger, Logging.Logger>();
+            builder.Services.AddScoped(s =>
+            {
+                var connectionString = builder.Configuration.GetSection("Azure:BlobConnectionString").Value;
+                var containerName = builder.Configuration.GetSection("Azure:ContainerName").Value;
+
+                var bsclient = new BlobServiceClient(connectionString);
+
+                return bsclient.GetBlobContainerClient(containerName);
+            });
+
+            builder.Services.Configure<FplOptions>(builder.Configuration.GetSection("Fpl"));
+            builder.Services.Configure<EmailOptions>(builder.Configuration.GetSection("Email"));
+            builder.Services.Configure<AzureOptions>(builder.Configuration.GetSection("Azure"));
+
+            // builder.Services.Configure<FplOptions>(options => builder.Configuration.Bind("Field", options));
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
